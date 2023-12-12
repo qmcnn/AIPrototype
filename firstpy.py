d0eba0d89d0cb0bef846eb2e14fb75f64ebfeebf
chanoknan <chanoknan.m@kkumail.com>
2023-12-12 10:20:58 +0700

    add function

--- firstpy.py
+++ firstpy.py
@@ -1,5 +1,11 @@
-#def main():
-#    print("Hello World!")
+def printHello():
+    print("Hello World!")
+
+def multiplyby9(inputV):
+    print(9*inputV)
 
 if __name__ == "__main__":
     print('we are in the main function')
+    multiplyby9(20)
+    printHello()
+    
