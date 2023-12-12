055610ac67154be306d8c942c1e086ca272ed0b3
chanoknan <chanoknan.m@kkumail.com>
2023-12-12 11:02:02 +0700

    add function

--- firstpy.py
+++ firstpy.py
@@ -1,3 +1,16 @@
+import argparse
+
+def parse_input():
+    parse = argparse.ArgumentParser()
+
+    parse.add_argument(
+        '--num'
+        type = int,
+        required = True,
+        help='input for the multiplyby9 function'
+    )
+
+
 def printHello():
     print("Hello World!")
 
@@ -5,7 +18,10 @@ def multiplyby9(inputV):
     print(9*inputV)
 
 if __name__ == "__main__":
+
+    input_v = parse_input()
+
+    print(f'the input num is {input_v.num}')
     print('we are in the main function')
     multiplyby9(20)
     printHello()
-    
