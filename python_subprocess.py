c3bc8f68509d189f955a35f259abde15584590a9
chanoknan <chanoknan.m@kkumail.com>
2023-12-25 15:33:42 +0700

    test process

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,40 +3,6 @@ import subprocess #สำหรับรัน terminal command
 if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"])
-    subprocess.run(["rm","-r","/home/chanoknan/testfolder1"])
+    subprocess.run(["python", "firstpy.py"])
+ 
 
-import argparse
-
-def parse_input():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        '--num',
-        type = int,
-        required = True,
-        help='input for the multiplyby9 function'
-    )
-
-    parser.add_argument(
-        '--XX',
-        type = int,
-        default = 7,
-        help='input for the multiplyby9 function'
-    )
-
-    args = parser.parse_args()
-    return args
-
-def printHello():
-    print("Hello World!")
-
-def multiplyby9(inputV):
-    print(9*inputV)
-
-if __name__ == "__main__":
-    input_v = parse_input()
-    print(f'the input XX is {input_v.XX}')
-    print(f'the input num is {input_v.num}')
-    print('we are in the main function')
-    multiplyby9(input_v.num) #แก้
-    printHello() 
