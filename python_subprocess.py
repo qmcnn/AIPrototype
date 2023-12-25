5e85e5e41bc110c3bba21f8a7dcfeefcc8545bbc
chanoknan <chanoknan.m@kkumail.com>
2023-12-25 16:09:38 +0700

    test process

--- python_subprocess.py
+++ python_subprocess.py
@@ -16,3 +16,10 @@ if __name__ == "__main__":
     print_()
  
 
+#use output from other program
+process_output = subprocess.Popen(["python", "firstpy.py", "--num", "0"],
+                                  stdout=subprocess.PIPE
+                                  stderr=subprocess.PIPE)
+out, err = process_output.communicate()
+print(out.decode('utf-8'))
+print(len(out.decode('utf-8')))
