e86c2044f37fe87f6137ac7443f47b42071b4391
chanoknan <chanoknan.m@kkumail.com>
2023-12-25 15:38:10 +0700

    test process

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,6 +3,6 @@ import subprocess #สำหรับรัน terminal command
 if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"])
-    subprocess.run(["python", "firstpy.py"])
+    subprocess.run(["python", "firstpy.py", "--num 100", "--XX 90"])
  
 
