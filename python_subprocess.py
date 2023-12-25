003cfab2391efe6cf6c3336255f98ed8f365c5c4
chanoknan <chanoknan.m@kkumail.com>
2023-12-25 15:46:54 +0700

    test process

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,8 +1,15 @@
 import subprocess #สำหรับรัน terminal command
 
+def print_():
+    print("-----------------------------------------")
 if __name__ == "__main__":
     #basic terminal command
     subprocess.run(["ls","-ltr"])
-    subprocess.run(["python", "firstpy.py", "--num 100", "--XX 90"])
+    subprocess.run(["python", "firstpy.py", "--num", "100", "--XX", "90"])
+    print_()
+    subprocess.run(["python", "firstpy.py", "--num", "-10", "--XX", "-90"])
+    print_()
+    subprocess.run(["python", "firstpy.py", "--num", "0"])
+    print_()
  
 
