69ac59d3902821fba6a770b8bb03c0c5998e826e
chanoknan <chanoknan.m@kkumail.com>
2024-01-02 09:55:34 +0700

    sum output

--- python_subprocess.py
+++ python_subprocess.py
@@ -26,6 +26,6 @@ print(len(out.decode('utf-8')))
 
 ##HW เขียน subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน hello world!)
 
-output1 = subprocess.run.(["python", "firstpy.py", "--num", "100","--XX", "90"])
+output1 = subprocess.run(["python", "firstpy.py", "--num", "100","--XX", "90"])
 value1 = int(output1.split('/n')[3])
 print(value1"sum output")
