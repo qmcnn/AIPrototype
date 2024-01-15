d9efbde0dacea690abb43babb58f963e774952ae
chanoknan <chanoknan.m@kkumail.com>
2024-01-15 15:20:05 +0700

    firstflask

---
+++ firstflask.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, render_template, make_response 
+import json
+#import pandas as pd
+
+app = Flask(__name__)
+
+@app.route("/")  
+def helloworld():
+    return "Hello, World!"
+
+if __name__ == "__name__":
+    app.run(host='0.0.0.0', debug=True, post=5001)#h
