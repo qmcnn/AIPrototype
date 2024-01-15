3b9d171cf1067c8c3e6937af1f5d1984d3b8dd4d
chanoknan <chanoknan.m@kkumail.com>
2024-01-15 15:42:45 +0700

    firstflask

--- firstflask.py
+++ firstflask.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def helloworld():
     return "Hello, World!"
 
+@app.route("/name")  
+def helloworld():
+    return "Hello, chanoknan!"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, post=5001)#h
