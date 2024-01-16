bd927dbde3b075ccc4b1cc8ca67bf4c0dedbdab6
chanoknan <chanoknan.m@kkumail.com>
2024-01-16 11:12:53 +0700

    form

--- firstflask.py
+++ firstflask.py
@@ -35,5 +35,31 @@ def homefn():
         print(lastnamein, file=sys.stdout)
         return render_template("home.html", name=namein)
 
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        #if 'file' not in request.files:
+        #    flash('No file part')
+        #    return redirect(request.url)
+        file = request.files['file']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        #if file.filename == '':
+        #    flash('No selected file')
+        #    return redirect(request.url)
+        file.save('filename')
+        return render_template("home.html", name='upload completed')
+        
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0',port=5001
