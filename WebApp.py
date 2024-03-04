2d7e34c4aac2c1d67b4e75def06f5a39bdcf3138
chanoknan <chanoknan.m@kkumail.com>
2024-03-04 14:42:50 +0700

    rm

--- WebApp.py
+++ WebApp.py
@@ -8,17 +8,12 @@ model = joblib.load("../AIPrototype2023/templates/model_webapp.joblib")
 app = Flask(__name__)
 scaler = StandardScaler()
 
-# Initialize an empty list to store predictions
-predictions = []
-
 @app.route('/myapp')
 def index():
     return render_template('index.html')
 
 @app.route('/prediction', methods=['GET', 'POST'])
 def upload_file():
-    global predictions  # Declare predictions as a global variable
-
     if request.method == 'POST':
         file = request.files['file']
         upload_folder = '../AIPrototype2023/static/data'
@@ -38,21 +33,8 @@ def upload_file():
         elif any(value == 1 for value in predictions):
             result_template = 'chronic.html'
 
-        # Write the predictions to a CSV file
-        csv_file_path = 'static/data/predictions.csv'
-        with open(csv_file_path, 'w') as f:
-            f.write('predict class\n')
-            for result in predictions:
-                f.write(f'{result}\n')
-
-        # Read the CSV file into a DataFrame
-        df = pd.read_csv(csv_file_path)
-
-        # Convert DataFrame to HTML table
-        table = df.to_html(index=False)
-
-        # Pass the predictions list and table to the template
-        return render_template(result_template, predictions=predictions, table=table)
+        # Pass the predictions list to the template
+        return render_template(result_template, predictions=predictions)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5001)
